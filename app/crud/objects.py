543a90ca962e9a1647a57d6c3607129babbd27a2
soumyadip <soumyadip@W10-L-ZQSPV2.windows.ebi.ac.uk>
2019-11-04 15:02:31 +0000

    Implementing review comments

--- app/crud/objects.py
+++ app/crud/objects.py
@@ -1,30 +1,25 @@
-from app.db.database import get_database
-from app.db.datamodels import objects, checksums, access_methods, contents
+from app.db.database import db
+from app.models.schema import objects, checksums, access_methods, contents
 
 
 async def get_db_objects(object_id: str):
-    database = await get_database()
     query = objects.select(objects.c.id == object_id)
-    return await database.fetch_one(query)
+    return await db.database.fetch_one(query)
 
 
 async def get_checksum(object_id: str):
-    database = await get_database()
     query = checksums.select(checksums.c.object_id == object_id)
-    return await database.fetch_all(query)
+    return await db.database.fetch_all(query)
 
 async def get_contents(object_id: str):
-    database = await get_database()
     query = contents.select(contents.c.object_id == object_id)
-    return await database.fetch_all(query)
+    return await db.database.fetch_all(query)
 
 async def get_object_access_methods(object_id: str):
-    database = await get_database()
     query = access_methods.select(access_methods.c.object_id == object_id)
-    return await database.fetch_all(query)
+    return await db.database.fetch_all(query)
 
 async def get_sub_objects(object_id: str):
-    database = await get_database()
     query = contents.select(contents.c.object_id == object_id)
-    return await database.fetch_all(query)
+    return await db.database.fetch_all(query)
     
