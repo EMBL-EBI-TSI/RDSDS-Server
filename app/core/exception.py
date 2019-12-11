878bc12445eb6528a75e7ad089bdd9e3fa6ab3f2
soumyadip <soumyadip@W10-L-ZQSPV2.windows.ebi.ac.uk>
2019-12-11 16:13:09 +0000

    exception printing

--- app/core/exception.py
+++ app/core/exception.py
@@ -3,7 +3,7 @@ from starlette.responses import JSONResponse
 
 async def http_exception_handler(request, exc):
     return JSONResponse(
-        {'msg': str(exc.detail), 'status_code': exc.status_code},
+        {'msg': str(exc), 'status_code': exc.status_code},
         status_code=exc.status_code
     )
     
