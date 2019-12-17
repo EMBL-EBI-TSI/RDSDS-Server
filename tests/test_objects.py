5b47222819acdc8db6b77c0f8ba553b4b399a5b4
soumyadip <soumyadip@W10-L-ZQSPV2.windows.ebi.ac.uk>
2019-12-17 11:35:25 +0000

    Implementing review comments

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -12,7 +12,15 @@ def test_get_object_success():
         assert response.status_code == 200
         drsObject = DrsObject(**response.json())
         assert drsObject.id == '0430d58d1199abc6310c7b51fedac148cc3b2d08c4c2dd5f40861e6ef930d7cf1502b92fa945a625acf80ef1dd2d3ea68d6b80e45043ec212f8f891fe2e48b91'
-        
+
+def test_get_bundle_success():
+    with TestClient(app) as api_client:
+        response = api_client.get("/ga4gh/drs/v1/objects/9b338448d613f4aed943d811a76d2350fc719b1bc334c0b64f442ed2f86848d07882958fb24000989a9986dcfaa492509ce02fb77586a60899544e9bda2dcfc1")
+        assert response.status_code == 200
+        drsObject = DrsObject(**response.json())
+        assert drsObject.id == '9b338448d613f4aed943d811a76d2350fc719b1bc334c0b64f442ed2f86848d07882958fb24000989a9986dcfaa492509ce02fb77586a60899544e9bda2dcfc1'
+
+
 def test_get_object_404():
     with TestClient(app) as api_client:
         response = api_client.get("/ga4gh/drs/v1/objects/test")
