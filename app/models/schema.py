ad9e5706ad01bf6164e0b577fa9b84ad90eadd76
soumyadip <soumyadip@W10-L-ZQSPV2.windows.ebi.ac.uk>
2020-03-09 11:19:49 +0000

    Adding datasets relation

--- app/models/schema.py
+++ app/models/schema.py
@@ -12,30 +12,27 @@ metadata = sqlalchemy.MetaData()
 objects = sqlalchemy.Table(
     "objects",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.String),
-    sqlalchemy.Column("name", sqlalchemy.String),
-    sqlalchemy.Column("description", sqlalchemy.String),
+    sqlalchemy.Column("id", sqlalchemy.String, primary_key=True),
     sqlalchemy.Column("self_uri", sqlalchemy.String),
     sqlalchemy.Column("size", sqlalchemy.BigInteger),
     sqlalchemy.Column("created_time", sqlalchemy.DateTime),
     sqlalchemy.Column("updated_time", sqlalchemy.DateTime),
-    sqlalchemy.Column("version", sqlalchemy.String),
-    sqlalchemy.Column("mime_type", sqlalchemy.String),
-    sqlalchemy.Column("aliases", sqlalchemy.String),
 )
 
 checksums = sqlalchemy.Table(
     "checksums",
     metadata,
-    sqlalchemy.Column("object_id", sqlalchemy.String),
+    sqlalchemy.Column("object_id", sqlalchemy.String, sqlalchemy.ForeignKey("objects.id")),
     sqlalchemy.Column("type", sqlalchemy.String),
     sqlalchemy.Column("checksum", sqlalchemy.String),
+    
+    sqlalchemy.UniqueConstraint('object_id', 'type', 'checksum', name='unique_checksum')
 )
 
 access_methods = sqlalchemy.Table(
     "access_methods",
     metadata,
-    sqlalchemy.Column("object_id", sqlalchemy.String),
+    sqlalchemy.Column("object_id", sqlalchemy.String, sqlalchemy.ForeignKey("objects.id")),
     sqlalchemy.Column("type", sqlalchemy.String),
     sqlalchemy.Column("access_url", sqlalchemy.String),
     sqlalchemy.Column("region", sqlalchemy.String),
@@ -46,11 +43,28 @@ access_methods = sqlalchemy.Table(
 contents = sqlalchemy.Table(
     "contents",
     metadata,
-    sqlalchemy.Column("object_id", sqlalchemy.String),
+    sqlalchemy.Column("object_id", sqlalchemy.String, sqlalchemy.ForeignKey("objects.id")),
     sqlalchemy.Column("id", sqlalchemy.String),
     sqlalchemy.Column("type", sqlalchemy.String),
     sqlalchemy.Column("name", sqlalchemy.String),
     sqlalchemy.Column("drs_uri", sqlalchemy.String),
+)
+
+datasets = sqlalchemy.Table(
+    "datasets",
+    metadata,
+    sqlalchemy.Column("object_id", sqlalchemy.String, sqlalchemy.ForeignKey("objects.id")),
+    sqlalchemy.Column("dataset", sqlalchemy.String),
+    sqlalchemy.Column("type", sqlalchemy.String),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("description", sqlalchemy.String),
+    sqlalchemy.Column("mime_type", sqlalchemy.String),
+    sqlalchemy.Column("bundle", sqlalchemy.String),
+    sqlalchemy.Column("version", sqlalchemy.Integer),
+    sqlalchemy.Column("created_time", sqlalchemy.DateTime),
+    sqlalchemy.Column("aliases", sqlalchemy.String),
+    
+    sqlalchemy.UniqueConstraint('object_id', 'dataset', 'bundle', 'version', name='unique_checksum')
 )
 
 metadata.create_all(engine)
