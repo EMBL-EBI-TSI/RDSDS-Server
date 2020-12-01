389a3db30fb4347ae6cc474d07d2b78ab1cffd58
JenkinsX <jenkinsx@ebi>
2020-12-01 15:47:04 +0000

    Fixing query

--- app/crud/datasets.py
+++ app/crud/datasets.py
@@ -1,7 +1,9 @@
 from app.db.database import db
 from app.models.schema import datasets
+from sqlalchemy import and_
 
 
 async def get_objects_by_dataset(dataset_id: str, bundle_id: str):
-    query = datasets.select(datasets.c.dataset == dataset_id, datasets.c.bundle == bundle_id)
+    query = datasets.select().where(and_(datasets.c.dataset == dataset_id, datasets.c.bundle == bundle_id))
+    print(query)
     return await db.database.fetch_all(query) 
