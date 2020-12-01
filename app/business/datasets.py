c30d09d1e3e1d00dc8296e9f1eb68fbfdf8c71f7
JenkinsX <jenkinsx@ebi>
2020-12-01 15:17:40 +0000

    Printing db result

--- app/business/datasets.py
+++ app/business/datasets.py
@@ -1,7 +1,7 @@
 from app.crud.datasets import get_objects_by_dataset
 
 async def get_objects_by_omics(dataset_id: str, bundle_id: str):
-    object_list= []
     object_list = get_objects_by_dataset(dataset_id,bundle_id)
+    print(object_list)
     return object_list
     
