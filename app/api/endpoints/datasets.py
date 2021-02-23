3bf885f1972b277c4f27b7a52c75da9243a34db6
soumyadip <soumyadip@ebi.ac.uk>
2021-02-23 17:28:37 +0000

    Adding scheme

--- app/api/endpoints/datasets.py
+++ app/api/endpoints/datasets.py
@@ -16,6 +16,7 @@ async def get_objects_by_omics(dataset_id: str, bundle_id: str, request: Request
     """Returns a list of Objects for the dataset
     """
     client_host = request.headers['host']
-    data = await datasets.get_objects_by_omics(dataset_id=dataset_id, bundle_id=bundle_id, client_host=client_host)
+    client_scheme = request.url.scheme
+    data = await datasets.get_objects_by_omics(dataset_id=dataset_id, bundle_id=bundle_id, client_host=client_host, client_scheme=client_scheme)
     print(data)
     return data
--- app/business/datasets.py
+++ app/business/datasets.py
@@ -2,7 +2,7 @@ from app.crud.datasets import get_objects_by_dataset
 from starlette.responses import JSONResponse
 from app.business.objects import get_objects
 
-async def get_objects_by_omics(dataset_id: str, bundle_id: str, client_host: str):
+async def get_objects_by_omics(dataset_id: str, bundle_id: str, client_host: str, client_scheme: str):
     object_list = await get_objects_by_dataset(dataset_id,bundle_id)
     if not object_list:
         return JSONResponse(status_code=404, content={
@@ -14,7 +14,7 @@ async def get_objects_by_omics(dataset_id: str, bundle_id: str, client_host: str
     for object in object_list:
       new_object = dict()
       new_object['drsURI'] = "drs://{}/{}".format(client_host, object['object_id'])
-      new_object['drsURL'] = "https://{}/ga4gh/drs/v1/objects/{}".format(client_host, object['object_id'])
+      new_object['drsURL'] = "{}://{}/ga4gh/drs/v1/objects/{}".format(client_scheme,client_host, object['object_id'])
       for key in object.keys():
         new_object[key] = object[key]
       print(new_object)
